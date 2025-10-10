d34939ec6b8a1560926491eb5a39216918c45afc
Thaynara.forte@escola.pr.gov.br <thaynara.forte@escola.pr.gov.br>
2025-10-10 08:04:03 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -3,3 +3,13 @@ criaCartao(
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
+criaCartao(
+    'Geografia'
+    'Qual a capital da França?',
+    "A capital da França é Paris"
+)
+cria Cartao(
+    'Programação'
+    'O que é uma função?',
+    'Uma função é o bloco de código que executa alguma tarefa'
+)
